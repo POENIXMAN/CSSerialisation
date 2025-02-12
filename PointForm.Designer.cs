dc095196ff971f8c1cf3153d51bd939ba4c77402
Ahmad Nasser <79102608+POENIXMAN@users.noreply.github.com>
2025-02-12 18:43:52 +0300

    Add files via upload

---
+++ PointForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FormsApp
+{
+    partial class PointForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSer = new System.Windows.Forms.Button();
+            this.btnDes = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(90, 335);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(108, 39);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(254, 335);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(108, 39);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnSer
+            // 
+            this.btnSer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSer.Location = new System.Drawing.Point(412, 335);
+            this.btnSer.Name = "btnSer";
+            this.btnSer.Size = new System.Drawing.Size(108, 39);
+            this.btnSer.TabIndex = 2;
+            this.btnSer.Text = "Serialize";
+            this.btnSer.UseVisualStyleBackColor = true;
+            this.btnSer.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // btnDes
+            // 
+            this.btnDes.Location = new System.Drawing.Point(576, 335);
+            this.btnDes.Name = "btnDes";
+            this.btnDes.Size = new System.Drawing.Size(108, 39);
+            this.btnDes.TabIndex = 3;
+            this.btnDes.Text = "Deserialize";
+            this.btnDes.UseVisualStyleBackColor = true;
+            this.btnDes.Click += new System.EventHandler(this.btnDeserialize_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(90, 53);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(594, 251);
+            this.listBox.TabIndex = 4;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            
+            // 
+            // PointForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnDes);
+            this.Controls.Add(this.btnSer);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "PointForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSer;
+        private System.Windows.Forms.Button btnDes;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ PointForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PointLib;
+
+namespace FormsApp
+{
+    public partial class PointForm : Form
+    {
+        private Point[] points = null;
+
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            points = new Point[5];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < points.Length; i++)
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+
+            listBox.DataSource = points;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (points == null)
+                return;
+
+            Array.Sort(points);
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin|YAML|*.yaml;*.yml";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json":
+                        var jf = new JsonSerializer();
+                        using (var w = new StreamWriter(fs))
+                            jf.Serialize(w, points);
+                        break;
+                    case ".yaml":
+                    case ".yml":
+                        var yamlSerializer = new YamlDotNet.Serialization.Serializer();
+                        using (var writer = new StreamWriter(fs))
+                        {
+                            yamlSerializer.Serialize(writer, points);
+                        }
+                        break;
+
+
+                }
+            }
+
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin|YAML|*.yaml;*.yml";
+
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json":
+                        var jf = new JsonSerializer();
+                        using (var r = new StreamReader(fs))
+                            points = (Point[])jf.Deserialize(r, typeof(Point[]));
+                        break;
+                    case ".yaml":
+                    case ".yml":
+                        var yamlDeserializer = new YamlDotNet.Serialization.Deserializer();
+                        using (var reader = new StreamReader(fs))
+                        {
+                            var rawPoints = yamlDeserializer.Deserialize<List<Dictionary<string, int>>>(reader);
+
+                            points = rawPoints.Select(dict =>
+                            {
+                                if (dict.ContainsKey("Z"))
+                                    return new Point3D(dict["X"], dict["Y"], dict["Z"]);
+                                else
+                                    return new Point(dict["X"], dict["Y"]);
+                            }).ToArray();
+                        }
+                        break;
+
+
+                }
+            }
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+    
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
